92c15e6f7e5f0d089288367a954aefb3f738dc82
ciaraosull <ciaraosull@gmail.com>
2022-05-31 21:56:44 +0000

    Create a filter panel & list display on admin panel

--- sensors/admin.py
+++ sensors/admin.py
@@ -2,4 +2,13 @@
 from django.contrib import admin
 from .models import Sensor
 
-admin.site.register(Sensor)
+
+@admin.register(Sensor)
+class SensorAdmin(admin.ModelAdmin):
+    """
+    Class to register the sensors to the admin panel
+    and display & filter specific information
+    """
+    list_display = ('sensor_id', 'country', 'city')
+    search_fields = ('sensor_id', 'date')
+    list_filter = ('sensor_id', 'date')
