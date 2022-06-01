f46fbeaa84959756e631f1141b2a6acbb03093ee
ciaraosull <ciaraosull@gmail.com>
2022-06-01 19:10:51 +0000

    Create GET & POST request of sensor data in views

--- sensors/views.py
+++ sensors/views.py
@@ -1,12 +1,26 @@
 """Create endpoints to access sensor data from """
-from django.shortcuts import render
 from django.http import JsonResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
 from .models import Sensor
 from .serializers import SensorSerializer
 
 
+@api_view(['GET', 'POST'])
 def sensor_list(request):
-    """Function to get all the sensor data, serialize and return json """
-    sensors = Sensor.objects.all()
-    serializer = SensorSerializer(sensors, many=True)
-    return JsonResponse({'sensors': serializer.data})
+    """
+    Function to take GET & POST request
+    of all the sensor data, serialize 
+    and return json
+    """
+    if request.method == 'GET':
+        sensors = Sensor.objects.all()
+        serializer = SensorSerializer(sensors, many=True)
+        return JsonResponse({'sensors': serializer.data})
+
+    if request.method == 'POST':
+        serializer = SensorSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
